44616e9e41443cff1685b279d3b1068bcbefdc98
WilsonPadua <onepersonrandom@gmail.com>
2025-11-10 16:43:04 -0300

    o segredo é manipular a frequencia de cada elemento

--- 05.tabelaDeFrequencia/Solucao.java
+++ 05.tabelaDeFrequencia/Solucao.java
@@ -5,32 +5,51 @@ public class Solucao{
 
     public static void main(String [] args){
         try{
-            BufferedReader reader = new BufferedReader(new InputStream(System.in));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String line = reader.readLine();
             String[] tokens = line.split(" ");
             int[] seq = new int[tokens.length];
 
             for(int i = 0; i < seq.length; i++){
-                seq[i] = Interger.parseInt(tokens[i]);
+                seq[i] = Integer.parseInt(tokens[i]);
             }
 
+        int[] freq = freq(seq);
+        String resultado = tabelaFreq(seq, freq);
 
+        System.out.println(resultado);
 
-
-        } catch (IOException) {}
+        } catch (IOException ioe) {}
     }
 
-    public int[] freq(int[] v){
+    public static int[] freq(int[] v){
         int maior = 0;
+        
         for(int e : v){
             if(e > maior) maior = e;
         }
 
-        int[] freq = new int[maior];
+        int[] freq = new int[maior +1];
 
-        for(int i = freq.length -1; i > 0; i--){
-                freq[i - maior] = v[i];
+        for(int i = 0; i < v.length; i++){
+                freq[v[i]] += 1;
         }
+
+        return freq;
+    }
+
+    public static String tabelaFreq(int[] seq, int[] freq){
+        String retorno = "";
+
+        for(int i = 0; i < seq.length; i++){
+            if(freq[seq[i]] > 0){
+                retorno += seq[i] + "," + freq[seq[i]] + " ";
+                freq[seq[i]] = 0;
+            }
+        }
+
+        return retorno.trim();
+
     }
 
 }
